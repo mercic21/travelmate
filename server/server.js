cf4f8965190e0fa3a9ac4f4a3ddf633a3a74f0d2
mercic21 <christianmerci11@gmail.com>
2025-06-12 15:37:01 +0200

    Updating CORS

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://travelmate-delta.vercel.app/', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
